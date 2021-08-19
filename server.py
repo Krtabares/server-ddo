8d8d3fc8b0523d19f37a421d5aeeaa47432312b2
DESKTOP-OE7O7H7\Tabares <krtabares@gmail.com>
2021-08-19 09:50:11 -0400

    json dumps

--- server.py
+++ server.py
@@ -424,7 +424,7 @@ async def insertUser(db, user):
                 COD_CIA = user['COD_CIA'],
                 GRUPO_CLIENTE = user['GRUPO_CLIENTE'],
                 COD_CLIENTE = user['COD_CLIENTE'],
-                permisos = user['permisos'],
+                permisos =json.dumps( user['permisos']),
                 estatus = user['estatus']
                 )
     print(sql)
