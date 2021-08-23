15e28f9d1ce2d419d4e2cb02b1958877326a11b1
DESKTOP-OE7O7H7\Tabares <krtabares@gmail.com>
2021-08-23 13:32:20 -0400

    fix

--- server.py
+++ server.py
@@ -415,7 +415,7 @@ async def insertUser(db, user):
                 \"{GRUPO_CLIENTE}\",
                 \"{COD_CLIENTE}\",
                 \"{estatus}\",
-                \"{permisos}\"
+                {permisos}
                 );
             """.format(
                 role = user['role'],
