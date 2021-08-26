8a35b9b476362ee21d78a3b80f6681b41bef1668
DESKTOP-OE7O7H7\Tabares <krtabares@gmail.com>
2021-08-25 22:56:41 -0400

    debug

--- server.py
+++ server.py
@@ -86,7 +86,7 @@ def getAPPconfig():
 
 pool = None
 def generate_session_pool():
-    pprint(conf)
+    pprint(conf['entorno'])
     if conf['entorno'] == "Desarrollo":
         # desarrollo
         dsn_tns = cx_Oracle.makedsn(
