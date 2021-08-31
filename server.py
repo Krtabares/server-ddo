8ba8bebc295853a3dce5129c4a76ca66636da456
DESKTOP-OE7O7H7\Tabares <krtabares@gmail.com>
2021-08-31 17:42:25 -0400

    buffered=True 2

--- server.py
+++ server.py
@@ -617,7 +617,7 @@ async def user_pass(request): # token: Token):
 #@jwt_required
 async def user_passID(request): # token: Token):
     db = get_mysql_db()
-    c = db.cursor()
+    c = db.cursor(buffered=True)
     query = """ SELECT id_usuarios, identificacion FROM `portal_ddo`.`usuarios` WHERE id_usuarios not in(1)"""
     c.execute(query)
     list = []
