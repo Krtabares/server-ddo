8397d72c03488d0a570abbbfcdc17803ab20dc2c
Kerby Tabares <kerby.tabares@tutenlabs.com>
2022-05-19 19:43:53 -0400

    last changes

--- server.py
+++ server.py
@@ -168,11 +168,7 @@ pool= mainInit(pool)
 async def print_on_request(request):
     global pool
     db = get_mysql_db()
-    # print("midleware")
-    # print(pool)
-    # pprint(db)
-    # db1 = get_oracle_db()
-    # pool.release(db1)
+    
     if 'dev' not in request.headers:
         if not pool:
             return response.json({"msg": "error"}, status=500)
